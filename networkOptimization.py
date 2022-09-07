3c01c2b53baa1d750420fdafc45aa080c650af87
Rishikesh Kushwaha <rishikesh.kushwaha@optym.com>
2022-09-07 19:42:02 +0530

    model to M

--- networkOptimization.py
+++ networkOptimization.py
@@ -1,12 +1,20 @@
 from pyomo.environ import ConcreteModel, Var, Objective, NonNegativeReals, Constraint
 from pyomo.environ import *
+import pandas as pd
 
 M = ConcreteModel()
-
+year = 2019
 M.I = Set(RangeSet(4095))
 M.J = Set(RangeSet(99))
 
+
+def demand_function(year):
+    forecast_demand = pd.read_csv('forecasted_demand_2019.csv')
+    return forecast_demand['2019']
+
+
 M.d = Param(M.I, rule=demand)
+
 M.x = Var([1,2], domain=NonNegativeReals)
 
 M.OBJ = Objective(expr = 2*M.x[1] + 3*M.x[2])
